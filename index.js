472f04fac1cabc615705cb072c96dd94fb171164
so sasaki <sosasaki0907@gmail.com>
2026-05-22 16:00:18 +0900

    0522ver1

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname,'public')));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use(session({
+    secret: 'secret key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 60 * 1000
+    }
+}));
 
 app.set('view engine', 'ejs');
 app.get('/', (req,res) => {
@@ -11,8 +25,22 @@ app.get('/', (req,res) => {
 app.get('/about', (req,res) => {
     res.render('about');
 });
+app.get('/login', (req,res) =>{
+    res.render('login');
+});
+
+app.post('/login', (req,res) => {
+    const { username, password } = req.body;
+    if(username === 'user' && password ==='password'){
+        req.session.username = username;
+        res.send('Login success');
+    } else {
+        res.send('Login failed');
+    }
+});
+
 app.use((req,res) => {
-    res.status(404),send('Not Found');
+    res.status(404).send('Not Found');
 });
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
